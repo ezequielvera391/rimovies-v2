669b3571974cfef5615bed74a2c0dc39917d5b53
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-13 14:41:46 -0300

    feat: page not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
