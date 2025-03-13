8f71c52eb6f7f4fb7bb7d255ff210ca7a6cb373e
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-13 14:54:22 -0300

    feat: create home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
