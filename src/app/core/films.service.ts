de9ba0f1ee2db18096ce0c0749774f6fb412d569
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-23 11:31:49 -0300

    feat: create section "recommended"

--- src/app/core/films.service.ts
+++ src/app/core/films.service.ts
@@ -4,20 +4,33 @@ import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Film } from './interfaces/films.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilmsService {
   private films$ = new BehaviorSubject<Film[]>([]);
-  private url = 'assets/mocks/movies.json' // TODO: traer de una variable de entorno
+  private recommendedFilms$ = new BehaviorSubject<Film[]>([]);
+  private url = 'assets/mocks/movies.json'; // TODO: traer de una variable de entorno
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   loadFilms(): Observable<Film[]> {
-    return this.http.get<Film[]>(this.url)
-      .pipe(tap(films => this.films$.next(films)));
+    return this.http
+      .get<Film[]>(this.url)
+      .pipe(tap((films) => this.films$.next(films)));
+  }
+
+  // TODO: implementar servicio que traiga recomendaciones en función a lo último visto
+  loadRecommendedFilms(): Observable<Film[]> {
+    return this.http
+      .get<Film[]>('assets/mocks/recommendedMovies.json')
+      .pipe(tap((films) => this.recommendedFilms$.next(films)));
   }
 
   getFilms(): Observable<Film[]> {
     return this.films$.asObservable();
+  }
+
+  getRecommendedFilms(): Observable<Film[]> {
+    return this.recommendedFilms$.asObservable();
   }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Film } from 'src/app/core/interfaces/films.interface';
+import { FilmsService } from 'src/app/core/films.service';
+import { FilmCardComponent } from "../films/film-card/film-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FilmCardComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  public recommendedFilms: Film[] = [];
 
+  constructor(private filmService: FilmsService) {}
+
+  ngOnInit(): void {
+    this.filmService.loadRecommendedFilms().subscribe({
+      next: (res: Film[]) => {
+        this.recommendedFilms = res;
+      },
+    });
+  }
 }
