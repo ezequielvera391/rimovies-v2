4bda70e6e9145bd0324d71477f1cb466fe59899c
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-23 11:32:08 -0300

    feat: add scroll animation to nav-bar component

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { SearchInputComponent } from "../search-input/search-input.component";
@@ -8,21 +8,27 @@ import { SearchInputComponent } from "../search-input/search-input.component";
   standalone: true,
   imports: [CommonModule, SearchInputComponent],
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
   public logoPath = 'assets/images/logos/logo-texto.png';
-  @Input() navLinks: {name: string, path:string}[] 
+  public isScrolled: boolean;
+  @Input() navLinks: { name: string; path: string }[];
 
-  constructor(private router: Router){}
+  constructor(private router: Router) {}
 
-  public redirectTo(path:string): void {
-    this.router.navigate([path])
+  public redirectTo(path: string): void {
+    this.router.navigate([path]);
   }
 
   public handleKeydown(event: KeyboardEvent, path: string): void {
     if (event.key === 'Enter' || event.key === ' ') {
       this.redirectTo(path);
     }
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
   }
 }
