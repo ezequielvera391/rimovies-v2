f0f0aec017e627440b78e110c7d994e7eb30d0ef
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-24 18:35:22 -0300

    feat: show cards in film component

--- src/app/features/films/films.component.ts
+++ src/app/features/films/films.component.ts
@@ -3,30 +3,31 @@ import { CommonModule } from '@angular/common';
 import { FilmsService } from 'src/app/core/films.service';
 import { Film } from 'src/app/core/interfaces/films.interface';
 import { SearchInputComponent } from "../../shared/search-input/search-input.component";
+import { FilmCardComponent } from './film-card/film-card.component';
 
 @Component({
   selector: 'app-films',
   standalone: true,
-  imports: [CommonModule, SearchInputComponent],
+  imports: [CommonModule, SearchInputComponent, FilmCardComponent],
   templateUrl: './films.component.html',
-  styleUrls: ['./films.component.scss']
+  styleUrls: ['./films.component.scss'],
 })
 export class FilmsComponent implements OnInit {
-  public films: Film[] = []
-  public filteredFilms: Film[] = []
-  public searchKeys: string[] = ['title', 'director.name', 'actors.name']
+  public films: Film[] = [];
+  public filteredFilms: Film[] = [];
+  public searchKeys: string[] = ['title', 'director.name', 'actors.name'];
   constructor(private filmService: FilmsService) {}
 
   ngOnInit(): void {
     this.filmService.getFilms().subscribe({
       next: (res: Film[]) => {
-        this.films = res
-        this.filteredFilms = this.films
-      }
+        this.films = res;
+        this.filteredFilms = this.films;
+      },
     });
   }
 
-  showFilteredFilms(filteredFilms:Film[]) {
-    this.filteredFilms = filteredFilms
+  showFilteredFilms(filteredFilms: Film[]) {
+    this.filteredFilms = filteredFilms;
   }
 }
