9c3a32f8429ffb7772999c4bc42bd5b75a678614
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-03-29 21:05:29 -0300

    feat: add navigation to detail page from film card

--- src/app/features/films/film-card/film-card.component.ts
+++ src/app/features/films/film-card/film-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseCardComponent } from "../../../shared/card/base-card.component";
 import { Router } from '@angular/router';
+import { Film } from 'src/app/core/interfaces/films.interface';
 
 @Component({
   selector: 'app-film-card',
@@ -10,21 +11,23 @@ import { Router } from '@angular/router';
   templateUrl: './film-card.component.html',
   styleUrls: ['./film-card.component.scss'],
 })
-export class FilmCardComponent {
+export class FilmCardComponent implements OnInit {
   public detailIcon: string;
   public favIcon: string;
-  @Input() imageUrl: string;
-  @Input() goToUrl: string;
-  @Input() id: string;
+  public goToUrl: string;
+  @Input() data: Film;
 
   constructor(private router: Router) {
     this.detailIcon = 'assets/images/icons/detail-eye.svg';
     this.favIcon = 'assets/images/icons/favorite.svg';
   }
+  ngOnInit(): void {
+    this.goToUrl = 'films/' + this.data.id;
+  }
 
   navigateTo(event: Event) {
-    event.stopPropagation(); // Evita que el `click` active la card base
-    this.router.navigate(['films/', this.id]);
+    event.stopPropagation();
+    this.router.navigate(['films/', this.data.id]);
   }
 
   addToFavorites(event: Event) {
