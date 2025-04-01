27f94189c0e29c702d6a0b90f3972178c27cbaa2
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-01 08:32:24 -0300

    feat: add chip component, button action, spacing new classes and other minors styles

--- src/app/core/playground/playground.component.ts
+++ src/app/core/playground/playground.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChipComponent } from 'src/app/shared/chip/chip.component';
 
 @Component({
   selector: 'app-playground',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChipComponent],
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.scss']
 })
---
+++ src/app/shared/chip/chip.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chip',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chip.component.html',
+  styleUrls: ['./chip.component.scss'],
+})
+export class ChipComponent {
+  @Input() label!: string;
+  @Input() removable: boolean = false;
+
+  remove() {
+    console.log(`Eliminar chip: ${this.label}`);
+  }
+}
