4e8ee9c1ef7f89f598cc4818c866d06d5f9f6906
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-10 20:23:21 -0300

    feat: implement ControlValueAccessor interface on password-input component

--- src/app/shared/password-input/password-input.component.ts
+++ src/app/shared/password-input/password-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-password-input',
@@ -8,15 +8,22 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './password-input.component.html',
   styleUrls: ['./password-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class PasswordInputComponent {
-  @Input() placeholder: string = 'Password';
-  @Input() label: string;
-  @Input() formControlName: string;
-  @Input() ariaLabel: string = 'Password';
-
+export class PasswordInputComponent implements ControlValueAccessor {
+  public value = '';
   public showPassword: boolean = false;
   public imgIcon: string;
+
+  @Input() placeholder: string = 'Password';
+  @Input() label: string;
+  @Input() ariaLabel: string = 'Password';
 
   ngOnInit(): void {
     this.imgIcon = 'assets/images/icons/visibility_off.svg';
@@ -27,5 +34,27 @@ export class PasswordInputComponent {
     this.showPassword
       ? (this.imgIcon = 'assets/images/icons/visibility_on.svg')
       : (this.imgIcon = 'assets/images/icons/visibility_off.svg');
+  }
+
+  private onChange = (_: any) => {};
+  private onTouched = () => {};
+
+  public writeValue(value: string): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public onInput(event: Event): void {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.value = newValue;
+    this.onChange(newValue);
+    this.onTouched();
   }
 }
