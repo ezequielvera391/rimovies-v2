b41977aae7b8a555def810a524567ff8a653152b
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-10 20:24:17 -0300

    feat: add unit test to login component

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -1,23 +1,100 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
-describe('LoginComponent', () => {
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [LoginComponent, ReactiveFormsModule, RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); // dispara el render del HTML
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the title "Welcome back!"', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain(
+      'Welcome back!'
+    );
+  });
+
+  it('should have an invalid form initially', () => {
+    expect(component.form.valid).toBeFalse();
+  });
+
+  it('should have a user input field', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const input = compiled.querySelector('[data-testid="username-input"]');
+    expect(input).toBeTruthy();
+  });
+
+  it('should have a password input field', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const input = compiled.querySelector('[data-testid="password-input"]');
+    expect(input).toBeTruthy();
+  });
+
+  it('should have a login button', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('[data-testid="login-button"]');
+    expect(button?.textContent).toContain('Login');
+  });
+
+  it('should disable the login button if the form is invalid', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector(
+      '[data-testid="login-button"]'
+    ) as HTMLButtonElement;
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should be valid when form is filled correctly', () => {
+    component.form.setValue({
+      username: 'testuser',
+      password: 'testpass',
+    });
+
+    expect(component.form.valid).toBeTrue();
+  });
+
+  it('sholud enable the login button if the form is valid', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector(
+      '[data-testid="login-button"]'
+    ) as HTMLButtonElement;
+    component.form.setValue({
+      username: 'testuser',
+      password: 'testpass',
+    });
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should have the correct routerLink for register', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const link = compiled.querySelector('[data-testid="register-link"]');
+    expect(link?.getAttribute('ng-reflect-router-link')).toBe('/auth/register');
+  });
+
+  it('should have a link to the reset password', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const link = compiled.querySelector('[data-testid="reset-link"]');
+    expect(link?.getAttribute('ng-reflect-router-link')).toBe(
+      '/auth/forgot-password'
+    );
+  });
+
+  // TODO: test submit behavior once implemented
+  xit('should handle form submission when valid', () => {
+    // To be implemented when submit logic is ready
   });
 });
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -2,12 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordInputComponent } from "../../../shared/password-input/password-input.component";
 import { RouterModule } from '@angular/router';
+import { FormBuilder,  FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, PasswordInputComponent, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule, PasswordInputComponent, RouterModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  get formValid(): boolean {
+    return this.form.valid
+  }
+}
