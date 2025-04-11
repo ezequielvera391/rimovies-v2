b1bbea4e746b2225949dd08f51b2742161b62b8c
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-11 10:54:40 -0300

    feat: add form to forgot-password inputs

--- src/app/features/auth/forgot-password/forgot-password.component.spec.ts
+++ src/app/features/auth/forgot-password/forgot-password.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ForgotPasswordComponent', () => {
+fdescribe('ForgotPasswordComponent', () => {
   let component: ForgotPasswordComponent;
   let fixture: ComponentFixture<ForgotPasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ ForgotPasswordComponent ]
+      imports: [ ForgotPasswordComponent, ReactiveFormsModule, RouterTestingModule ]
     })
     .compileComponents();
 
@@ -20,4 +22,90 @@ describe('ForgotPasswordComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the title "Reset yout password"', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain(
+      'Reset yout password'
+    );
+  });
+
+  it('should have an invalid form initially', () => {
+    expect(component.form.valid).toBeFalse();
+  });
+
+  it('should have a user input field', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const input = compiled.querySelector('[data-testid="email-input"]');
+    expect(input).toBeTruthy();
+  });
+
+  it('should have a reset button', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector('[data-testid="reset-button"]');
+    expect(button?.textContent).toContain('Continue');
+  });
+
+  it('should disable the login button if the form is invalid', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector(
+      '[data-testid="reset-button"]'
+    ) as HTMLButtonElement;
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should be valid when form is filled correctly', () => {
+    component.form.setValue({
+      email: 'testuser@emial.com',
+    });
+
+    expect(component.form.valid).toBeTrue();
+  });
+
+  it('sholud enable the reset button if the form is valid', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const button = compiled.querySelector(
+      '[data-testid="reset-button"]'
+    ) as HTMLButtonElement;
+    component.form.setValue({
+      email: 'testuser@emial.com',
+    });
+    fixture.detectChanges();
+    expect(button.disabled).toBeFalse();
+  });
+
+  it('should have the correct routerLink for register', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const link = compiled.querySelector('[data-testid="register-link"]');
+    expect(link?.getAttribute('ng-reflect-router-link')).toBe('/auth/register');
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
