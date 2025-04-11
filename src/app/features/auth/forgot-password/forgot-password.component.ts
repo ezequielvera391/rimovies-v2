dab2632caba685dcfd10bad15f1e6d512c10e83e
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-11 10:54:57 -0300

    feat: add unit test to forgot-password component

--- src/app/features/auth/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/forgot-password/forgot-password.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
+  public form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  public handleSubmit(): void {
+    const { email } = this.form.value;
+    console.log(email)
+  }
+
+  get formValid(): boolean {
+    return this.form.valid;
+  }
 }
