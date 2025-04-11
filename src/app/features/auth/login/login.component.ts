9c4ca64af98537f24ed05ac1254c0ee519791e27
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-10 22:40:00 -0300

    feat: add handleSubmit

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordInputComponent } from "../../../shared/password-input/password-input.component";
 import { RouterModule } from '@angular/router';
@@ -11,14 +11,29 @@ import { FormBuilder,  FormGroup, ReactiveFormsModule, Validators } from '@angul
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public form: FormGroup;
 
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]]
+  constructor(private fb: FormBuilder) {
+    this.createForm()
+  }
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((res)=> {
+      console.log(res)
     })
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  public handleSubmit(): void {
+    const { username, password } = this.form.value;
+    console.log({ username, password });
   }
 
   get formValid(): boolean {
