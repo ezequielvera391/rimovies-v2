a138ddd3bad4a9c508f5073215a244912d061663
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-12 09:10:28 -0300

    feat: add reactive form to register component

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -2,12 +2,45 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordInputComponent } from 'src/app/shared/password-input/password-input.component';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { matchFieldsValidator } from 'src/app/core/utils/validators-custom.utils';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, PasswordInputComponent, RouterModule],
+  imports: [
+    CommonModule,
+    PasswordInputComponent,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+    },
+    { validators: matchFieldsValidator('password', 'password2', 'passwordMismatch') });
+  }
+
+  public handleSubmit(): void {
+    if (this.form.invalid) return;
+    const { username, password, password2 } = this.form.value;
+    console.log({ username, password, password2 });
+  }
+
+  get formValid(): boolean {
+    return this.form.valid;
+  }
+}
+
