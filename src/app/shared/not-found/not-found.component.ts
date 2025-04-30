9017c4d72d62542a647c6521ed9f30cb2537108a
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-04-30 14:51:03 -0300

    feat: create 404 not found page

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -9,5 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+  constructor(private router: Router){}
 
+  public redirectToHome() {
+    this.router.navigate([''])
+  }
 }
