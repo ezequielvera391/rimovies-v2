bd347c9df8fe3638e6e0ab8681ef28c5284a8d83
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2025-09-02 10:26:45 -0300

    feat: add logout button

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,7 +15,7 @@ export class NavBarComponent {
   public isScrolled: boolean;
   @Input() navLinks: { name: string; path: string }[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   public redirectTo(path: string): void {
     this.router.navigate([path]);
@@ -24,6 +25,17 @@ export class NavBarComponent {
     if (event.key === 'Enter' || event.key === ' ') {
       this.redirectTo(path);
     }
+  }
+
+  public logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/auth/login']);
+      },
+      error: (err) => {
+        console.error('Logout failed', err);
+      },
+    })
   }
 
   @HostListener('window:scroll')
